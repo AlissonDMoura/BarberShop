3f7d9af7437f25a1cc8a79c0010ffe391ae6350f
Alisson Moura <alissonmaster@hotmail.com>
2020-11-08 08:56:22 +0000

    V1
    Creation of Login page
    Need to figure out better object names later.

--- Barber_Shop/src/barber_shop/Barber_Shop.java
+++ Barber_Shop/src/barber_shop/Barber_Shop.java
@@ -5,6 +5,10 @@
  */
 package barber_shop;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author aliss
@@ -15,7 +19,20 @@ public class Barber_Shop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JLabel email = new JLabel("E-mail");
+        
+        frame.setSize(500, 480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+        
+        panel.setLayout(null);
+        
+        
+        
     }
     
 }
