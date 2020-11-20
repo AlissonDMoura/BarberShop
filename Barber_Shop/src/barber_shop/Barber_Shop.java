9e1a786965af7828f556b99a02a1fe5e1d61c983
Alisson Moura <alissonmaster@hotmail.com>
2020-11-20 20:06:49 +0000

    V.05
    Add Radio Buttons into Register page.
    Add Customer Log Page and it's elements.

--- Barber_Shop/src/barber_shop/Barber_Shop.java
+++ Barber_Shop/src/barber_shop/Barber_Shop.java
@@ -11,11 +11,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 /**
@@ -30,10 +32,11 @@ public class Barber_Shop extends JFrame {
     public static void main(String[] args) {
         
        
-       dbConector.getConnection();
+       //dbConector.getConnection();
         
-        //LoginPage();
+        // LoginPage();
         //RegisterPage();
+        //CustomerPage();
                 
     }
     
@@ -50,7 +53,8 @@ public class Barber_Shop extends JFrame {
         JButton registerButton = new JButton("Register");
         //Frame created, Panel created, E-mail label created
         
-        frame.setSize(500, 480);
+        frame.setSize(440, 180);
+        frame.setTitle("Login into BarberShop");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         frame.setVisible(true);
@@ -75,15 +79,17 @@ public class Barber_Shop extends JFrame {
         
         loginButton.setBounds(300, 30, 100, 20);
                 
-        registerButton.setBounds(200, 100, 100, 100);
+        registerButton.setBounds(90, 100, 200, 30);
         //
     }
     
     private static void RegisterPage(){
             
+        
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         
+        //All Labels
         JLabel labEmail = new JLabel("E-mail");
         JLabel labPassword = new JLabel("Password");
         JLabel labPassword2 = new JLabel("Confirm Password");
@@ -91,8 +97,8 @@ public class Barber_Shop extends JFrame {
         JLabel labSurname = new JLabel("Surname");
         JLabel labPhone = new JLabel("Phone");
         JLabel labLocation = new JLabel("Location");
-        //All Labels
         
+        //All Text fields and password Fields
         JTextField txtEmail = new JTextField(20);
         JTextField txtName = new JTextField(20);
         JTextField txtSurname = new JTextField(20);
@@ -100,27 +106,36 @@ public class Barber_Shop extends JFrame {
         JTextField txtLocation = new JTextField(20);
         JPasswordField txtPassword = new JPasswordField(20);
         JPasswordField txtPassword2 = new JPasswordField(20);
-        //All Text fields and password Fields
         
+        //Regular Buttons 
         JButton LoginPageButton = new JButton("Go Back");
         JButton registerButton = new JButton("Register me");
-        JButton barberUser = new JButton("I'm a Barber");
-        JButton customerUser = new JButton ("I'm a Customer");
-        //All buttons
         
+        //Radio buttons group together for further selection.
+        String[] userType = {"Hairdresser", "Customer"};
+        JRadioButton hairDButton = new JRadioButton(userType[0]);
+        JRadioButton customerButton = new JRadioButton(userType[1]);
+        ButtonGroup typeSelect = new ButtonGroup();
+        customerButton.setSelected(true);
+        typeSelect.add(hairDButton);
+        typeSelect.add(customerButton);
+        
+        
+        //Frame Settings
         frame.setSize(350, 500);
+        frame.setTitle("Register into BarberShop's");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         frame.setVisible(true);
-        //
         
+        //Panel visisible, and layout null, so I can position every item by coordinates.
         panel.setVisible(true);
         panel.setLayout(null);
 
+        
             
         
-        //
-        
+        //Add labels and text boxes onto the panel, and position the coordinates.
         labName.setBounds(20, 50, 80, 20);
         txtName.setBounds(110, 50, 200, 20);
         panel.add(labName);
@@ -156,19 +171,61 @@ public class Barber_Shop extends JFrame {
         panel.add(labPassword2);
         panel.add(txtPassword2);
         
+        //Add Buttons to the panel and set coordinates.
         panel.add(registerButton);
         registerButton.setBounds(110, 280, 130, 50);
         
-        panel.add(barberUser);
-        barberUser.setBounds(20, 20, 130, 20);
-        
-        panel.add(customerUser);
-        customerUser.setBounds(180, 20, 130, 20);
+        panel.add(hairDButton);
+        panel.add(customerButton);
+        hairDButton.setBounds(50, 20, 100, 20);
+        customerButton.setBounds(180, 20, 100, 20);
         
         LoginPageButton.setBounds(110, 370, 130, 50);
         panel.add(LoginPageButton);
         
         
+        frame.validate();
+        frame.repaint();
+    }
+
+    private static void CustomerPage() {
+        
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        
+        frame.setSize(400, 300);
+        frame.setTitle("Customer + Customer name");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setVisible(true);
+        
+        
+        
+        
+        JLabel welcome = new JLabel("Welcome, + Customer Name + Today is + Today's Date.");
+        JLabel welcome2 = new JLabel("What would you like to do?");
+        
+                JButton searchHd = new JButton("Search Hairdresser");
+        JButton viewApp = new JButton("My Appointments");
+        JButton SubFb = new JButton ("Submit a Feedback");
+        JButton Logout = new JButton ("Logout");
+        
+        panel.add(welcome);
+        welcome.setBounds(30, 10, 400, 20);
+        panel.add(welcome2);
+        welcome2.setBounds(100, 30, 400, 20);
+        
+        panel.add(searchHd);
+        searchHd.setBounds(100, 70, 150, 40);
+        
+        panel.add(SubFb);
+        SubFb.setBounds(100, 110, 150, 40);
+        
+        panel.add(viewApp);
+        viewApp.setBounds(100, 150, 150, 40);
+        
+        panel.add(Logout);
+        Logout.setBounds(295, 230, 80, 20);
         
     }
     }
